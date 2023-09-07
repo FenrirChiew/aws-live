4717b1eed3f4bfce52217a455d612feb38ab3159
FenrirChiew <86295813+FenrirChiew@users.noreply.github.com>
2023-09-07 18:47:43 +0800

    Add files via upload

---
+++ config.py
@@ -0,0 +1,7 @@
+customhost = "employee.c5ol7u6zg3le.us-east-1.rds.amazonaws.com"
+customuser = "aws_user"
+custompass = "Bait3273"
+customdb = "employee"
+custombucket = "chiewhongkuang-employee"
+customregion = "us-east-1"
+
